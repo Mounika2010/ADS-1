b6e0617e4cb5a5ae57fc4b2490b0684f4cd6605e
Mounika2010 <mangalagirimounika20@msitprogram.net>
2018-09-28 02:06:29 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ m4/4.1 Balanced Paranthesis/4.1 Balanced Paranthesis/ParenthesisClient.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class ParenthesisClient {
+	public static boolean main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int testcases = Integer.parseInt(scan.nextLine());
+		Stack<Character> stack = new Stack<Character>();
+		for (int i = 0; i < testcases; i++) {
+			String line = scan.nextLine();
+			int j = 0;
+			for (j = 0; j < line.length(); j++) {
+				char ch = line.charAt(j);
+				if (ch == '('|| ch == '[' || ch == '{') {
+					stack.push(ch);
+				
+				} else {
+					if (stack.isEmpty()) {
+						return false;
+					}
+					if (ch == '}' ) {
+						stack.pop();
+					} else if (ch == ']' && stack.top() == '[') {
+						stack.pop();
+
+					} else {
+						return false;
+					}
+				}
+				
+			}
+			return stack.isEmpty();
+		}
+	}
+}

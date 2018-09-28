642ee5f627efdb85bb659e655dafff759394bef8
Mounika2010 <mangalagirimounika20@msitprogram.net>
2018-09-28 15:30:17 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m4/4.1 Balanced Paranthesis/4.1 Balanced Paranthesis/ParenthesisClient.java
+++ m4/4.1 Balanced Paranthesis/4.1 Balanced Paranthesis/ParenthesisClient.java
@@ -1,33 +1,48 @@
 import java.util.*;
 public class ParenthesisClient {
-	public static void main(String[] args) throws Exception {
-		Scanner scan = new Scanner(System.in);
-		int testcases = Integer.parseInt(scan.nextLine());
-		for (int i = 0; i < testcases; i++) {
-			String line = scan.nextLine();
-			Stack<Character> stack = new Stack<Character>();
-			int j = 0;
-			for (j = 0; j < line.length(); j++) {
-				char ch = line.charAt(j);
-				if (ch == '('|| ch == '[' || ch == '{') {
-					stack.push(ch);
-				
-				} else {
-					if (stack.isEmpty() == false && ch == ')' && stack.top() == '(')
-						stack.pop();
-					else if (stack.isEmpty() == false && ch == ']' && stack.top() == '[')
-						stack.pop();
-					else if (stack.isEmpty() == false && ch == '}' && stack.top() == '{')
-						stack.pop();
-					else 
-						break;
-				}
-			}
-			if (j == line.length() && stack.isEmpty())
-				System.out.println("YES");
-			else
-				System.out.println("NO");
+    /**
+     * Main function for parenthesis Client.
+     *
+     * @param      args       The arguments
+     *
+     * @throws     Exception  { exception_description }
+     */
+    public static void main(final String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        int testcases = Integer.parseInt(scan.nextLine());
+        for (int i = 0; i < testcases; i++) {
+            String line = scan.nextLine();
+            Stack<Character> stack = new Stack<Character>();
+            int j = 0;
+            for (j = 0; j < line.length(); j++) {
+                char ch = line.charAt(j);
+                if (ch == '(' || ch == '[' || ch == '{') {
+                    stack.push(ch);
+                
+                } else {
+                    if (stack.isEmpty() == false && ch == ')' && stack.top() == '(') {
+                        stack.pop();
+                    }
+                    else if (stack.isEmpty() == false && ch == ']' && stack.top() == '[') {
+                        stack.pop();
+                    }
+                    else if (stack.isEmpty() == false && ch == '}' && stack.top() == '{') {
+                        stack.pop();
+                    }
+                    else{
+                        break;
+                    }
+                }
+            }
+            if (j == line.length() && stack.isEmpty()) {
+                System.out.println("YES");
+            }
+            else{
+                System.out.println("NO");
+            }
 
-		}
-	}
+        }
+    }
 }
+
+

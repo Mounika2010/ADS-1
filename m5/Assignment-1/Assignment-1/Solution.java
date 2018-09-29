c140addc451c78f9a65aba14bf54d98f1555232e
Mounika2010 <mangalagirimounika20@msitprogram.net>
2018-09-29 12:46:11 +0530

    "check_style_score -> 0 of 12 passed. style score: 0/1 "

--- m5/Assignment-1/Assignment-1/Solution.java
+++ m5/Assignment-1/Assignment-1/Solution.java
@@ -27,65 +27,110 @@ import java.util.Scanner;
 
 // }
 
-class LinkedList {
-	private Node start;
-	private int size;
+// class LinkedList {
+// 	Node start;
+// 	Node last;
+// 	int size;
 
-	LinkedList() { 
+// 	LinkedList() { 
+// 		this.start = null;
+// 		this.size = 0;
+// 	}
+// class Node {
+// 	String data;
+//     Node next;
+   
+//     Node(String data) {
+
+// 		this.data = data;
+// 	}
+
+// 	public int size() {
+// 		return this.size;
+// 	}
+// }
+
+// 	public boolean isEmpty() {
+// 		return first == null;
+
+// 	}
+
+// 	public void push(int item) {
+// 		Node temp = new Node();
+// 		// if (temp == null) {
+// 		// 	throw new Exception("Stack is full");
+// 		// }
+
+// 		temp.data = item;
+// 		temp.next = first;
+// 		first = temp;
+// 	}
+
+// 	public int pop() {
+// 	// if (first == null) {
+// 	// 		throw new  NoSuchElementException("Stack is empty");
+// 	// 	}
+// 	int data = first.data;
+// 	first = first.next;
+// 	size--;
+// 	return data;
+// 	}
+
+// }
+class LinkedList {
+    Node start = null;
+    Node last = null;
+    int size = 0;
+
+    LinkedList() { 
 		this.start = null;
 		this.size = 0;
 	}
+    class Node {
+        String data;
+        Node next;
+        Node(String data) {
+            this.data = data;
+        }
+    }
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    public void push(String item) {
+        Node newnode = new Node(item);
+        if (isEmpty()) {
+            start = newnode;
+            last = start;
+            last.next = null;
+            size++;
+            return;
+        }
 
-	public int size() {
-		return this.size;
-	}
+    }
+    public String pop() {
+        if (isEmpty()) {
+            return null;
+        }
+        String item = start.data;
+        start = start.next;
+        size--;
+        return item;
 
-	private static class Node {
-		private int data;
-		private Node next;
-	}
-
-	private Node first;
-	public boolean isEmpty() {
-		return first == null;
-
-	}
-
-	public void push(int item) {
-		Node temp = new Node();
-		// if (temp == null) {
-		// 	throw new Exception("Stack is full");
-		// }
-
-		temp.data = item;
-		temp.next = first;
-		first = temp;
-	}
-
-	public int pop() {
-	// if (first == null) {
-	// 		throw new  NoSuchElementException("Stack is empty");
-	// 	}
-	int data = first.data;
-	first = first.next;
-	size--;
-	return data;
-	}
+}
 }
 
 class AddLargeNumbers {
     
- //    public static LinkedList numberToDigits(String number) {
+    public static LinkedList numberToDigits(String number) {
 
- //    	LinkedList linkl = new LinkedList();
- //        String[] digits = number.split("");
- //        // String[] digits = new String[](number);
- //        for (int i = 0; i < digits.length; i++) {
- //            linkl.push(digits[i]);
- //        }
- //        return linkl;
-	// }
-	
+    	LinkedList linkl = new LinkedList();
+        String[] digits = number.split("");
+        // String[] digits = new String[](number);
+        for (int i = 0; i < digits.length; i++) {
+            linkl.push(digits[i]);
+        }
+        return linkl;
+	}
   //   	if (start == null) {
 		// 	Node newnode = new Node(number);
 		// 	start = newnode;
@@ -124,15 +169,15 @@ public class Solution {
         String q = sc.nextLine();
         switch(input){
             case "numberToDigits":
-                // LinkedList pDigits = AddLargeNumbers.numberToDigits(p);
-                // LinkedList qDigits = AddLargeNumbers.numberToDigits(q);
-                // System.out.println(AddLargeNumbers.digitsToNumber(pDigits));
-                // System.out.println(AddLargeNumbers.digitsToNumber(qDigits));
+                LinkedList pDigits = AddLargeNumbers.numberToDigits(p);
+                LinkedList qDigits = AddLargeNumbers.numberToDigits(q);
+                System.out.println(AddLargeNumbers.digitsToNumber(pDigits));
+                System.out.println(AddLargeNumbers.digitsToNumber(qDigits));
                 break;
 
             case "addLargeNumbers":
-                // pDigits = AddLargeNumbers.numberToDigits(p);
-                // qDigits = AddLargeNumbers.numberToDigits(q);
+                pDigits = AddLargeNumbers.numberToDigits(p);
+                qDigits = AddLargeNumbers.numberToDigits(q);
                 // LinkedList result = AddLargeNumbers.addLargeNumbers(pDigits, qDigits);
                 // System.out.println(AddLargeNumbers.digitsToNumber(result));
                 break;

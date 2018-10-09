88ea61bed5d57c61986a152ac4699b245e339b32
Mounika2010 <mangalagirimounika20@msitprogram.net>
2018-10-09 20:07:05 +0530

    "ads1_m14_a1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ m14/Assignment 1/14.1 Dynamic Median Finding/14.1 Dynamic Median Finding/Solution.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class Solution {
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        MinPQ<Float> minpq = new MinPQ<Float>(n);
+        MaxPQ<Float> maxpq = new MaxPQ<Float>(n);
+        
+        Float median = 0.0f;
+        for (int i = 0; i < n; i++) {
+            Float val = scan.nextFloat();
+            if (val > median) {
+                minpq.insert(val);
+            } else {
+                maxpq.insert(val);
+            }
+
+            if (maxpq.size() - minpq.size() > 1) {
+                maxpq.insert(minpq.delMin());
+            }
+
+            if (minpq.size() == maxpq.size()) {
+                median = (minpq.min() + maxpq.max())/2;
+            } 
+
+            else if (minpq.size() > maxpq.size()) {
+                median = maxpq.max();
+            }
+
+            median = minpq.min();
+
+        }
+
+    }
+}

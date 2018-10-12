c5e54d2fddcec47d3a35b330f8cda833c6785432
Mounika2010 <mangalagirimounika20@msitprogram.net>
2018-10-12 12:33:20 +0530

    "ads1_m16_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ m16/16.1 BST/16.1 BST/Book.java
@@ -0,0 +1,47 @@
+public class Book {
+
+	String bookname;
+	String authorname;
+	double price;
+
+	Book(String bookname, String authorname, double price) {
+		this.bookname = bookname;
+		this.authorname = authorname;
+		this.price = price;
+	}
+
+	public String getBookname() {
+		return this.bookname;
+	}
+
+	public String getAuthorname() {
+		return this.authorname;
+	}
+
+	public double getprice() {
+		return this.price;
+	}
+
+	public void setBookname(String bookname1) {
+		this.bookname = bookname1;
+	}
+
+	public void setAuthorname(String authorname1) {
+		this.authorname = authorname1;
+	}
+
+	public void setPrice(double price1) {
+		this.price = price1;
+	}
+
+	public int compareTo(Book that) {
+		if (this.getAuthorname().compareTo(that.getAuthorname()) > 0) {
+			return 1;
+		} else if (this.getAuthorname().compareTo(that.getAuthorname()) < 0) {
+			return -1;
+		} else {
+			return 0;
+		}
+	}
+
+}
